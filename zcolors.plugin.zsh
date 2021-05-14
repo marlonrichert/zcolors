e52ef151cee6dc4e7ff7b96df1f0eab615e40481
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-05-14 20:05:10 +0300

    Fix bugs in completion description colors

--- zcolors.plugin.zsh
+++ zcolors.plugin.zsh
@@ -5,10 +5,10 @@
   autoload -Uz ${${(%):-%x}:h}/functions/*~*.zwc
 
   typeset -g zle_highlight=(
-    isearch:fg=0,bg=11  # black on bright yellow
-    paste:none
-    region:bg=12        # bright blue
-    special:fg=14,bold  # bright cyan
-    suffix:bg=12        # bright blue
+    isearch:fg=black,bg=11  # bright yellow
+    paste:bold
+    region:bg=blue,fg=15    # bright white
+    special:fg=14           # bright cyan
+    suffix:bg=blue,fg=15    # bright white
   )
 }

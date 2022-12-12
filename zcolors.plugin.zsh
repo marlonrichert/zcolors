b75b23e7edd984ed3776cfb18c46dcec63c123b8
Marlon Richert <marlonrichert@users.noreply.github.com>
2022-12-12 14:05:14 +0200

    Fix bug in autoload code

--- zcolors.plugin.zsh
+++ zcolors.plugin.zsh
@@ -3,7 +3,7 @@ zcolors.plugin() {
   emulate -L zsh; setopt extendedglob warncreateglobal
   autoload -Uz add-zsh-hook
 
-  autoload -Uz ${${(%):-%x}:P:h}/functions/*~*.zwc
+  autoload -Uz ${${(%):-%x}:P:h}/functions/[[:alpha:]]*~*.zwc
 
   typeset -g zle_highlight=(
     isearch:fg=black,bg=11  # bright yellow
@@ -12,19 +12,30 @@ zcolors.plugin() {
     region:bg=blue,fg=15    # bright white
     suffix:bg=blue,fg=15    # bright white
   )
-  export GREP_COLOR='30;103'
+
+  export GREP_COLOR='30;103'  # black on bright yellow
+
   if command -v less > /dev/null && (( ${${=$( less -V )}[2]} >= 581 )); then
+    # -D configures colors.
+    # S = search results
+    # P = prompt at the bottom of the screen
+    # First letter is foreground, second letter is background.
+    # $ separates multiple instance of the same flag.
     export -TU LESS less ' '
-    less+=(  --use-color -DSkY )
+    less+=( --use-color '-DSkY$DPWB'  )
   fi
 
+  # TODO: Let the plugin itself generate and cache the output of zcolors.
+  # Invalidate when Git index is newer than cache or $LS_COLORS is newer than the one in cache.
+  # If LS_COLORS is empty or not set, store the default $LS_COLORS in cache.
+
+  add-zsh-hook precmd .zcolors.precmd
   .zcolors.precmd() {
     add-zsh-hook -d precmd .zcolors.precmd
     unfunction .zcolors.precmd
     zmodload -F zsh/parameter p:saliases
     typeset -g ls_colors=( $ls_colors[@] '*.'${(@k)^saliases}=${${$( lscolor sg )#$'\e['}%m} )
   }
-  add-zsh-hook precmd .zcolors.precmd
 }
 
 {
